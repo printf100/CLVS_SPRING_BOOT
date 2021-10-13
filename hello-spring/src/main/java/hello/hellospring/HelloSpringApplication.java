3fc0405ca5c0b0addb5589440d3cee097725289f
printf100 <mickey0502@naver.com>
2021-10-13 12:34:21 +0900

    20211013 modify hellospringapplication.java

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "false");
-		System.setProperty("spring.devtools.livereload.enabled", "true");
-
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
